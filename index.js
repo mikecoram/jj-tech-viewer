58ebeb6e00e9b3c5783b46ca4c4801bb455f85e2
Mike Coram Personal <Mike-Personal@Mike-Mbp.lan>
2018-09-23 16:44:45 +0100

    Update

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 
-const fetch = require("node-fetch");
+(async () => {
 
-const techCsv = fetch("file://./techs.csv");
+    const csvResponse = await fetch("https://mikecoram.github.io/jj-tech-viewer/techs.csv");
+    
+    const csvString = await readfile(await csvResponse.blob())
 
-const reader = new FileReader()
+    parseCsvString(csvString);
+})();
+
+function parseCsvString(csvString) {
+    for (let line of csvString.split("\n")) {
+        for (let column of line.split(",")) {
+            let cleanColumn = column.replace("\"", "");
+            console.log(cleanColumn)
+        }
+    }
+}
+
+function readfile(blob) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+
+        reader.onload = e => {
+            resolve(reader.result);
+        }
+
+        reader.onerror = e => {
+            reject();
+        }
+
+        reader.readAsText(blob);
+    });
+}
