c4dc78f27ba9f87cdf1ac3d75e6cc4c88c3a00d8
Mike Coram Personal <Mike-Personal@Mike-Mbp.lan>
2018-09-23 19:08:32 +0100

    Add filtering and search

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let techniques;
+let currentTechniques;
 let tags;
 let types;
 let contexts;
@@ -6,13 +7,26 @@ let fighterAs;
 let fighterBs;
 
 (async () => {
+
     techniques = await getTechniques();
+
     tags = getTags(techniques);
     types = getField(techniques, "techniqueType");
     startingPositions = getField(techniques, "startingPosition");
     contexts = getField(techniques, "videoType");
     fighterAs = getField(techniques, "fighterA");
     fighterBs = getField(techniques, "fighterB");
+
+    document.getElementById("filter-form").innerHTML += makeSelect(contexts, "Context (all)", "context");
+    document.getElementById("filter-form").innerHTML += makeSelect(startingPositions, "Starting Position (all)", "startingPosition");
+    document.getElementById("filter-form").innerHTML += makeSelect(types, "Type (all)", "type");
+
+    populateTechniquesTable(techniques);
+
+})();
+
+function populateTechniquesTable(techniques) {
+    document.getElementById("tech-list").innerHTML = "";
 
     for (let tech of techniques) {
         let rowHtml = "";
@@ -30,23 +44,68 @@ let fighterBs;
         document.getElementById("tech-list").innerHTML += newRow;
     }
 
-})();
+}
 
-function makeSelect(options, field) {
+function makeSelect(options, defaultOption, fieldName) {
     let optionsHtml = "";
+
+    optionsHtml += `<option value="none-selected">${defaultOption}</option>`;
+
     for (let o of options) {
         optionsHtml += `<option value="${o}">${o}</option>`;
     }
 
-    return `<select>
+    return `<select id="select-${fieldName}" onchange="refreshTable()">
                 ${optionsHtml}
             </select>`;
 }
 
+function refreshTable() {
+
+    applyFilters();
+
+    populateTechniquesTable(currentTechniques);
+
+}
+
+function applyFilters() {
+    const context = document.getElementById("select-context").selectedOptions[0].value;
+    const startingPosition = document.getElementById("select-startingPosition").selectedOptions[0].value;
+    const type = document.getElementById("select-type").selectedOptions[0].value;
+
+    currentTechniques = techniques;
+
+    if (context != "none-selected") {
+        currentTechniques = currentTechniques.filter(t => t.videoType == context);
+    }
+
+    if (startingPosition != "none-selected") {
+        currentTechniques = currentTechniques.filter(t => t.startingPosition == startingPosition);
+    }
+
+    if (type != "none-selected") {
+        currentTechniques = currentTechniques.filter(t => t.techniqueType == type);
+    }
+
+    return currentTechniques;
+}
+
 function viewGif(id) {
     const tech = techniques.find(t => t.id === id);
-    const row = document.getElementById(`tech-${id}`);
-    document.getElementById("gif-view").innerHTML = `<img src="${tech.gifLink}" />`;
+    document.getElementById("gif-view").innerHTML = `<img width=560 height=315 src="${tech.gifLink}" onclick="dismissGif()" />`;
+}
+
+function dismissGif() {
+    document.getElementById("gif-view").innerHTML = "";
+}
+
+function search() {
+    let term = document.getElementById("search-term").value;
+
+    currentTechniques = applyFilters().filter(t => t.techniqueName.toLowerCase().includes(term.toLowerCase())
+        || t.tags.toLowerCase().includes(term.toLowerCase()));
+
+    populateTechniquesTable(currentTechniques);
 }
 
 const IncludeColumns = [
