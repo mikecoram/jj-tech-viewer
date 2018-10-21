e435599c9f2ba9d2d5fcc5e1b1eb607c8a1b957b
mikecoram <mikecoram96@gmail.com>
2018-10-21 19:53:10 +0100

    Init react app

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div>Hello</div>
+    );
+  }
+}
+
+export default App;
